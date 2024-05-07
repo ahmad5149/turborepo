09de8849d565b6b9126d1605a53462b29eb5eec3
Muhammad Ahmad <muhammadahmad@nxb.com.pk>
2024-05-07 16:11:46 +0500

    Added one more shared component

--- my-turborepo/apps/docs/app/page.tsx
+++ my-turborepo/apps/docs/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import Image from 'next/image';
+import { Card } from '@repo/ui/card';
+import { Code } from '@repo/ui/code';
+import styles from './page.module.css';
+import { Button } from '@repo/ui/button';
+import { Shared } from '@repo/ui/shared';
 
 function Gradient({
   conic,
@@ -22,32 +23,32 @@ function Gradient({
         className,
       ]
         .filter(Boolean)
-        .join(" ")}
+        .join(' ')}
     />
   );
 }
 
 const LINKS = [
   {
-    title: "Docs",
-    href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.',
   },
   {
-    title: "Learn",
-    href: "https://turbo.build/repo/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.',
   },
   {
-    title: "Templates",
-    href: "https://turbo.build/repo/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
+    title: 'Templates',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.',
   },
   {
-    title: "Deploy",
-    href: "https://vercel.com/new",
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
     description:
-      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
+      'Instantly deploy your Turborepo to a shareable URL with Vercel.',
   },
 ];
 
@@ -65,7 +66,7 @@ export default function Page(): JSX.Element {
             rel="noopener noreferrer"
             target="_blank"
           >
-            By{" "}
+            By{' '}
             <Image
               alt="Vercel Logo"
               className={styles.vercelLogo}
@@ -81,6 +82,7 @@ export default function Page(): JSX.Element {
       <Button appName="docs" className={styles.button}>
         Click me!
       </Button>
+      <Shared />
 
       <div className={styles.hero}>
         <div className={styles.heroContent}>
@@ -91,7 +93,7 @@ export default function Page(): JSX.Element {
                 height={614}
                 src="circles.svg"
                 width={614}
-                style={{ pointerEvents: "none" }}
+                style={{ pointerEvents: 'none' }}
               />
             </div>
             <div className={styles.logoGradientContainer}>
@@ -105,7 +107,7 @@ export default function Page(): JSX.Element {
                 priority
                 src="turborepo.svg"
                 width={120}
-                style={{ pointerEvents: "none" }}
+                style={{ pointerEvents: 'none' }}
               />
             </div>
           </div>
---
+++ my-turborepo/packages/ui/src/Shared.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import React from 'react';
+
+export const Shared = () => {
+  return <h1>Test shared component</h1>;
+};
